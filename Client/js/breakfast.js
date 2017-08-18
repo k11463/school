26f787c0c765183a803a35c3a864368445d85ce1
k11463 <k7766134@gmail.com>
2017-08-18 09:27:35 +0800

    20170818

--- Client/js/breakfast.js
+++ Client/js/breakfast.js
@@ -1,28 +1,56 @@
 function chgopt(){
     $("#brk_select option").remove();
     var a = $("input[name=brk]:checked").val();
-    console.log(a);
     for(var i = 0; i < opt[a].length; i++){
         $("#brk_select").append("<option value='" + opt[a][i].price + "'>" + opt[a][i].name + "</option>");
     }
     chgipt();
 }
 
-
-
 function chgipt(){
     var a = $("#brk_select option:selected").val();
     var b = $("#count").val();
     $("#price").val(a);
     $("#total01").val(a * b);
+    var c = a * b;
+    c_alltotal();
+}
+
+function chgbox(){
+  var a = $("#soto").prop("checked");
+  var b = $("#plus").prop("checked");
+  var c = $("#soto").val();
+  var d = $("#plus").val();
+  var total = 0;
+  if (a){
+    $("#bag").val(c);
+  }
+  else{
+    $("#bag").val(0);
+  }
+  if (b){
+    $("#pp").val(d);
+  }
+  else{
+    $("#pp").val(0);
+  }
+  c_alltotal();
+}
+
+function c_alltotal(){
+  var a = $("#pp").val()/100 + 1;
+  var total = ($("#total01").val()*1 + $("#bag").val()*1) * a;
+  $("#alltotal").val(total);
 }
 
 function core_breakfast(){
+    $("#alltotal").val(alltotal);
     chgopt();
     chgipt();
-    $("input").on("change", chgopt);
+    $("input[type=radio]").on("change", chgopt);
     $("#brk_select").on("change", chgipt);
     $("#count").on("change", chgipt);
+    $("input[type=checkbox]").on("change", chgbox);
 }
 
 var opt = [
@@ -39,7 +67,7 @@ var opt = [
             "name": "豆漿",
             "price": 12
         }
-            
+
     ],
     [
         {
